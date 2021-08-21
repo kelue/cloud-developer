51b1de772641772b43bd574d6c7db6120b73afc9
kelue <austincmoon5@gmail.com>
2021-08-21 15:35:34 +0100

    stTODO exercise image filter working

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { Request, Response } from 'express';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
 (async () => {
@@ -26,7 +27,24 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  
+  // app.get("/filteredimage", async (req, res) => {
+  //   const image_url = req.query.image_url;
+  //   if (!image_url) {
+  //     return res.status(422).send("Please provide an image URL")
+  //   } else {
+  //     const filteredpath = await filterImageFromURL(image_url)
+  //     res.status(200).sendFile(filteredpath, () => deleteLocalFiles([filteredpath]))
+  //   }
+  // } );
 
+  app.get("/filteredimage", async (req:Request, res:Response) => {
+    let {image_url} = req.query;
+
+    const filteredpath = await filterImageFromURL(image_url)
+    return res.status(200).sendFile(filteredpath, async ()=> deleteLocalFiles([filteredpath]))
+
+  } );
   /**************************************************************************** */
 
   //! END @TODO1
